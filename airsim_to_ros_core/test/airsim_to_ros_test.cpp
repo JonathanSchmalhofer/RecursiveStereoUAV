3ee4f641d5740f4548e69c9ab237cebfa5776676
Jonathan Bryan Schmalhofer <Jonathan.Schmalhofer@bmw.de>
2017-08-10 17:06:16 +0200

    added unit tests for air_sim_to_ros_core

---
+++ airsim_to_ros_core/test/airsim_to_ros_test.cpp
@@ -0,0 +1,28 @@
+///
+/// @file
+/// @copyright Copyright (C) 2017, Jonathan Bryan Schmalhofer
+///
+/// @brief Collection of Unit Tests for the AirSimToRos Node
+///
+
+#include <gtest/gtest.h>
+#include <ros/time.h>
+#include "airsimros_airsim_to_ros/airsim_to_ros.h"
+
+TEST(AirSimToRosTestSuite, DefaultStatus)
+{
+    // ARRANGE
+    auto airsim_to_ros = airsimros::AirSimToRos();
+
+    // ACT
+    auto airsim_to_ros_status = airsim_to_ros.GetStatus();
+
+    // ASSERT
+    ASSERT_EQ(airsim_to_ros_status, 0);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
