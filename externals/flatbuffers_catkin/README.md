d2d020384b1a6a34b82c8440d4614dd7db9e5929
Jonathan Bryan Schmalhofer <jonathan.schmalhofer@mytum.de>
2017-08-15 14:43:09 +0200

    added and integrated flatbuffers#

---
+++ externals/flatbuffers_catkin/README.md
@@ -0,0 +1,4 @@
+flatbuffers_catkin
+==================
+
+A catkin wrapper for FlatBuffers: https://github.com/google/flatbuffers/
--- js_airsim_to_ros_node/src/airsim_to_ros_node.cpp
+++ js_airsim_to_ros_node/src/airsim_to_ros_node.cpp
@@ -2,6 +2,13 @@
 #include "ros/ros.h"
 #include "js_airsim_to_ros_library/airsim_to_ros_class.h"
 #include "zeromq_cpp/zmq.hpp"
+#include "StarBuffers_generated.h"
+
+struct Star {
+    double radius ;
+    double mass;
+    double volume;
+};
 
 int main(int argc, char **argv)
 {
@@ -17,6 +24,19 @@ int main(int argc, char **argv)
     subscriber.setsockopt(ZMQ_IDENTITY, "Hello", 5);
     subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
     subscriber.connect("tcp://localhost:5565");
+    
+    zmq::message_t receiveMessage;
+    subscriber.recv(&receiveMessage);
+
+    flatbuffers::FlatBufferBuilder fbb;
+    StarBufferBuilder builder(fbb);
+
+    auto star = GetStarBuffer(receiveMessage.data());
+
+    std::cout << "Received Star" << std::endl;
+    std::cout << "radius: " << star->radius() << std::endl;
+    std::cout << "mass: " << star->mass() << std::endl;
+    std::cout << "volume: " << star->volume() << std::endl;
 
 
     ros::Rate r(5);
