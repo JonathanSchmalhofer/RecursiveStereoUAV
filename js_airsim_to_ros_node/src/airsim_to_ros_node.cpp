aeb274d6c1e427edff84a24a7a3f663cb6427550
Jonathan Bryan Schmalhofer <jonathan.schmalhofer@mytum.de>
2017-08-15 12:59:15 +0200

    integrated zeromq_catkin into js_airsim_ros_node

--- js_airsim_to_ros_node/src/airsim_to_ros_node.cpp
+++ js_airsim_to_ros_node/src/airsim_to_ros_node.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include "ros/ros.h"
 #include "js_airsim_to_ros_library/airsim_to_ros_class.h"
+#include "zeromq_cpp/zmq.hpp"
 
 int main(int argc, char **argv)
 {
@@ -8,6 +9,15 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     js_airsim_to_ros_library::AirSimToRosClass airsim_to_ros;
+    
+    zmq::context_t context(1);
+
+    //  Connect our subscriber socket
+    zmq::socket_t subscriber (context, ZMQ_SUB);
+    subscriber.setsockopt(ZMQ_IDENTITY, "Hello", 5);
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+    subscriber.connect("tcp://localhost:5565");
+
 
     ros::Rate r(5);
 
