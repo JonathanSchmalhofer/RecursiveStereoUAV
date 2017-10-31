a243cdb8255345911ba4aea49f3b160f3ae3f278
Jonathan Schmalhofer <jonathan.schmalhofer@mytum.de>
2017-10-31 20:09:30 +0100

    renamed sender and receiver to client and bridge

--- airsim_ros_interface/src/airsim_zmq_bridge.cpp
+++ airsim_ros_interface/src/airsim_zmq_bridge.cpp
@@ -1,6 +1,16 @@
-#include "StereoImageRpcClient.hpp" // only for the AirSim includes needed
-#include "ProcessSynchronizationQueue.hpp"
+#include "StereoImageRpcClient.hpp"
+#include "js_airsim_to_ros_library/Image_generated.h"
+#include "zeromq_cpp/zmq.hpp"
 
+// Synchronization
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
+#include <iostream>
+#include <cstring>
+#include "trace_queue.hpp"
+
+// Vector
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/containers/vector.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
@@ -9,86 +19,144 @@
 
 using namespace boost::interprocess;
 
-//Alias an STL compatible allocator of ints that allocates ints from the managed
-//shared memory segment.  This allocator will allow to place containers
-//in managed shared memory segments
-typedef allocator<msr::airlib::VehicleCameraBase::ImageResponse, managed_shared_memory::segment_manager> ShmemAllocator;
+//Define an STL compatible allocator of ints that allocates from the managed_shared_memory.
+//This allocator will allow placing containers in the segment
+typedef allocator<std::uint8_t, managed_shared_memory::segment_manager>  SharedMemoryAllocator;
 
-//Alias a vector that uses the previous STL-like allocator
-typedef vector<msr::airlib::VehicleCameraBase::ImageResponse, ShmemAllocator> ImageResponseVector;
+//Alias a vector that uses the previous STL-like allocator so that allocates
+//its values from the segment
+typedef vector<std::uint8_t, SharedMemoryAllocator> ImageVector;
 
-#include <boost/interprocess/shared_memory_object.hpp>
-#include <boost/interprocess/mapped_region.hpp>
-#include <boost/interprocess/sync/scoped_lock.hpp>
-#include <iostream>
-#include <cstring>
+
 
 int main(int argc, const char *argv[])
 {
-    std::cout << "Press ENTER to start ZeroMQ bridge application" << std::endl; std::cin.get();
     //Create a shared memory object.
-    shared_memory_object shm
-       (open_only                    //only create
-       ,"shared_memory_mutex"              //name
-       ,read_write                   //read-write mode
-       );
+    shared_memory_object mutex_shared_memory(open_only, "MutexSharedMemory", read_write);
     
-    try{
+    zmq::context_t context(1);
+
+    //  We send updates via this socket
+    zmq::socket_t publishSocket = zmq::socket_t(context, ZMQ_PUB);
+    publishSocket.bind("tcp://*:5676");
+    
+    try
+    {
         //Map the whole shared memory in this process
-        mapped_region region
-           (shm                       //What to map
-           ,read_write //Map it as read-write
-           );
-        
+        mapped_region region(mutex_shared_memory, read_write);
+
         //Get the address of the mapped region
-        void * addr       = region.get_address();
-         
+        void *region_address = region.get_address();
+
         //Obtain a pointer to the shared structure
-        ProcessSynchronizationQueue * data = static_cast<ProcessSynchronizationQueue*>(addr);
+        trace_queue *mutex_data = static_cast<trace_queue*>(region_address);
         
-        //Print messages until the other process marks the end
-        bool end_loop = false;
+        bool keep_looping = true;
         do
         {
-            scoped_lock<interprocess_mutex> lock(data->mutex_);
-            if(!data->message_available_)
+            scoped_lock<interprocess_mutex> lock(mutex_data->mutex_);
+            if(!mutex_data->message_available_)
             {
-                data->condition_empty_.wait(lock);
+                mutex_data->condition_empty_.wait(lock);
             }
             else
             {
-                // PROCESSING COMES HERE
-                // TODO
-                
                 //Open the managed segment
-                managed_shared_memory segment(open_only, "shared_memory_stereo_images_and_depth_image");
-                
+                managed_shared_memory segment(open_only, "VectorSharedMemory");
+
                 //Find the vector using the c-string name
-                ImageResponseVector *stereo_image_depth_image_vector = segment.find<ImageResponseVector>("ImageResponseVector").first;
+                ImageVector *myvector = segment.find<ImageVector>("ImageVector").first;
+
+                //std::cout << "myvector->size() = " << myvector->size() << std::endl;
+                //std::cout << "myvector->at(0) = " << unsigned(myvector->at(0)) << std::endl;
                 
-                /*
-                 * TODO: Do we need this?
-                //When done, destroy the vector from the segment
-                segment.destroy<ImageResponseVector>("ImageResponseVector");
-                */
+                std::vector<msr::airlib::VehicleCameraBase::ImageResponse> received_image_response_vector;
                 
-                std::cout << "Number of images is " << unsigned(stereo_image_depth_image_vector->size()) << std::endl;
-                
-                //Notify the other process that the buffer is empty
-                data->message_available_ = false;
-                data->condition_full_.notify_one();
+                while(mutex_data->number_sub_messages_ > 0)
+                {
+                    if(!mutex_data->message_available_)
+                    {
+                        std::cout << "Waiting" << std::endl;
+                        mutex_data->condition_empty_.wait(lock);
+                    }
+                    
+                    std::cout << "mutex_data->number_sub_messages_ = " << unsigned(mutex_data->number_sub_messages_) << std::endl;
+                    
+                    msr::airlib::VehicleCameraBase::ImageResponse sub_message_image;
+                    for(int i = 0; i < myvector->size(); ++i)
+                    {
+                        sub_message_image.image_data_uint8.push_back(myvector->at(i));
+                    }
+                    received_image_response_vector.push_back(sub_message_image);
+                    std::cout << "myvector->size() = " << myvector->size() << std::endl;
+                    std::cout << "myvector->at(0) = " << unsigned(myvector->at(0)) << std::endl;
+                    
+                    /////////////////////////////////////////////////
+                    const std::uint8_t header_stamp_sec = 0;
+                    const std::uint8_t header_stamp_nsec = 0;
+                    const std::uint8_t header_seq = 0;
+                    const std::string  header_frame_id = "";
+                    const std::uint8_t  image_height = 0;
+                    const std::uint8_t  image_width = 0;
+                    const std::string  image_encoding = "";
+                    const bool  image_is_bigendian = false;
+                    const std::uint8_t  image_step = 0;
+                    
+                    flatbuffers::FlatBufferBuilder fbb;
+                    
+                    // Image.header.stamp
+                    airsim_to_ros::time message_time(mutex_data->message_image_information_.header_stamp_sec_,mutex_data->message_image_information_.header_stamp_nsec_);
+                    
+                    // Image.header
+                    auto header = airsim_to_ros::CreateHeader(
+                        fbb, 
+                        mutex_data->message_image_information_.header_seq_,
+                        &message_time,
+                        fbb.CreateString(mutex_data->message_image_information_.header_frame_id_));
+                    fbb.Finish(header);
+                    
+                    // Image
+                    //std::vector<std::uint8_t> image_data(3 * 640 * 480);
+                    auto image = airsim_to_ros::CreateImage(
+                        fbb, 
+                        header, 
+                        mutex_data->message_image_information_.image_height_,
+                        mutex_data->message_image_information_.image_width_,
+                        fbb.CreateString(mutex_data->message_image_information_.image_encoding_),
+                        mutex_data->message_image_information_.image_is_bigendian_,
+                        mutex_data->message_image_information_.image_step_,
+                        fbb.CreateVector<std::uint8_t>(sub_message_image.image_data_uint8));
+                    fbb.Finish(image);
+                    
+                    int buffersize = fbb.GetSize();
+                    zmq::message_t image_msg(buffersize);
+                    memcpy((void *)image_msg.data(), fbb.GetBufferPointer(), buffersize);
+                    publishSocket.send(image_msg);
+                    std::cout << "Sent flatbuffer message via zmq: " << buffersize << std::endl;
+                    std::cout << "    buffersize: " << buffersize << std::endl;
+                    std::cout << "    height: " << unsigned(mutex_data->message_image_information_.image_height_) << std::endl;
+                    std::cout << "    width: " << unsigned(mutex_data->message_image_information_.image_width_) << std::endl;
+                    std::cout << "    step: " << unsigned(mutex_data->message_image_information_.image_step_) << std::endl;
+                    std::cout << "    encoding: " << mutex_data->message_image_information_.image_encoding_ << std::endl;
+                    /////////////////////////////////////////////////
+                    
+                    //Notify the other process that the buffer is empty
+                    mutex_data->number_sub_messages_--;
+                    mutex_data->message_available_ = false;
+                    mutex_data->condition_full_.notify_one();
+                }
+                std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+                mutex_data->message_available_ = false;
+                mutex_data->condition_full_.notify_one();
             }
         }
-        while(!end_loop);
+        while(keep_looping);
     }
     catch(interprocess_exception &ex)
     {
-        shared_memory_object::remove("shared_memory_mutex");
         std::cout << ex.what() << std::endl;
         return 1;
     }
-    
-    //Erase shared memory
-    shared_memory_object::remove("shared_memory_mutex");
+
     return 0;
 }
