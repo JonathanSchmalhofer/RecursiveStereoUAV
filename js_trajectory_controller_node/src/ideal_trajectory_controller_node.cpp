52738f86845740020ed6025ca901d7637bb9393d
Jonathan Bryan Schmalhofer <jonathan.schmalhofer@mytum.de>
2017-12-08 21:28:55 +0100

    added initial trajectory control node

--- js_trajectory_controller_node/src/ideal_trajectory_controller_node.cpp
+++ js_trajectory_controller_node/src/ideal_trajectory_controller_node.cpp
@@ -15,12 +15,28 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "ideal_trajectory_controller_node");
     ros::NodeHandle node_handle;
     
-    js_trajectory_controller_node::Trajectory3DPointToAirSimClass("tcp://*:5676");
+    js_trajectory_controller_node::Trajectory3DPointToAirSimClass ros_to_airsim("tcp://*:6677");
     
     ROS_INFO("Starting node");
 
+    double z = 0.0f;
+
+    ros::Rate node_rate(0.1); // 0.1 hz
     while (ros::ok())
     {
+ 	z -= 0.1f;
+        ros_to_airsim.SetTrajectory3DPointTimeStampSec(0);
+        ros_to_airsim.SetTrajectory3DPointTimeStampNsec(0);
+        ros_to_airsim.SetTrajectory3DPointPosePositionX(0);
+        ros_to_airsim.SetTrajectory3DPointPosePositionY(0);
+        ros_to_airsim.SetTrajectory3DPointPosePositionZ(z);
+        ros_to_airsim.SetTrajectory3DPointPoseOrientationX(0);
+        ros_to_airsim.SetTrajectory3DPointPoseOrientationY(0);
+        ros_to_airsim.SetTrajectory3DPointPoseOrientationZ(0);
+        ros_to_airsim.SetTrajectory3DPointPoseOrientationW(1);
+        ros_to_airsim.SendTrajectory3DPoint();
+        ros::spinOnce();
+        node_rate.sleep();
     }
 
     return 0;
--- js_trajectory_controller_node/src/trajectory_point_to_airsim_class.cpp
+++ js_trajectory_controller_node/src/trajectory_point_to_airsim_class.cpp
@@ -60,38 +60,47 @@ Trajectory3DPointToAirSimClass::~Trajectory3DPointToAirSimClass()
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointTimeStampSec(std::uint32_t sec)
 {
+    trajectory3dpoint_timestamp_sec_ = sec;
 }
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointTimeStampNsec(std::uint32_t nsec)
 {
+    trajectory3dpoint_timestamp_nsec_ = nsec;
 }
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointPosePositionX(double x)
 {
+    trajectory3dpoint_pose_position_x_ = x;
 }
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointPosePositionY(double y)
 {
+    trajectory3dpoint_pose_position_y_ = y;
 }
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointPosePositionZ(double z)
 {
+    trajectory3dpoint_pose_position_z_ = z;
 }
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointPoseOrientationX(double x)
 {
+    trajectory3dpoint_pose_orientation_x_ = x;
 }
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointPoseOrientationY(double y)
 {
+    trajectory3dpoint_pose_orientation_y_ = y;
 }
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointPoseOrientationZ(double z)
 {
+    trajectory3dpoint_pose_orientation_z_ = z;
 }
 
 void Trajectory3DPointToAirSimClass::SetTrajectory3DPointPoseOrientationW(double w)
 {
+    trajectory3dpoint_pose_orientation_w_ = w;
 }
 }  // namespace js_airsim_to_ros_library
 
