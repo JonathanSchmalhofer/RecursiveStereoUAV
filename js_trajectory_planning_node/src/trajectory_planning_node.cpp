1bdbf38cd9b1f2efbebb3f69833f83f789a90c41
Jonathan Bryan Schmalhofer <jonathan.schmalhofer@mytum.de>
2017-12-19 22:36:18 +0100

    added publishing of empty trajectory3d message

--- js_trajectory_planning_node/src/trajectory_planning_node.cpp
+++ js_trajectory_planning_node/src/trajectory_planning_node.cpp
@@ -6,17 +6,23 @@
 ///
 #include <string>
 #include <ros/ros.h>
+#include <js_messages/Trajectory3D.h>
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "trajectory_planning_node");
     ros::NodeHandle node_handle;
     
+    ros::Publisher trajectory3d_publisher = node_handle.advertise<js_messages::Trajectory3D>("/trajectory_planning/trajectory3d", 1000);
+    
     ROS_INFO("Starting node");
 
-    ros::Rate node_rate(25.0f); // 25.0 hz
+    ros::Rate node_rate(2.0f); // 2.0 hz
     while (ros::ok())
     {
+        js_messages::Trajectory3D trajectory3d_message;
+        trajectory3d_publisher.publish(trajectory3d_message);
+        
         ros::spinOnce();
         node_rate.sleep();
     }
