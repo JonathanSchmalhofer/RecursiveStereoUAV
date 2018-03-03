12bd4b447f29c448fe6533e56e1b674a243c8eca
Jonathan Bryan Schmalhofer <jonathan.schmalhofer@mytum.de>
2018-03-03 16:23:53 +0100

    test for grid square location added

--- robojackets_3d_trajectory_planning_node/test/obstacle_grid_test.cpp
+++ robojackets_3d_trajectory_planning_node/test/obstacle_grid_test.cpp
@@ -12,7 +12,7 @@ namespace RRT
 TEST(ObstacleGrid, Instatiate)
 {
     double width = 50.0f, height= 60.0f, depth = 70.0f;
-    int discretized_width = 52, discretized_height= 63, discretized_depth = 74;
+    int discretized_width = 42, discretized_height= 63, discretized_depth = 74;
     ObstacleGrid obstacle_grid(width,
                                height,
                                depth,
@@ -30,8 +30,22 @@ TEST(ObstacleGrid, Instatiate)
 
 TEST(ObstacleGrid, GetGridSquareForLocation)
 {
-}
+    double width = 10.0f, height= 10.0f, depth = 10.0f;
+    int discretized_width = 10, discretized_height= 10, discretized_depth = 10;
+    ObstacleGrid obstacle_grid(width,
+                               height,
+                               depth,
+                               discretized_width,
+                               discretized_height,
+                               discretized_depth);
 
+    Eigen::Vector3d continues_point(0.4,0.5,2.4);
+    Eigen::Vector3i discrete_point = obstacle_grid.GetGridSquareForLocation(continues_point);
+    EXPECT_EQ(discrete_point.x(), 0);
+    EXPECT_EQ(discrete_point.y(), 1);
+    EXPECT_EQ(discrete_point.z(), 2);
+}
+/*
 TEST(ObstacleGrid, Initialize)
 {
 }
@@ -62,7 +76,7 @@ TEST(ObstacleGrid, InsertedMeasurements)
 
 TEST(ObstacleGrid, CheckCollisionFreeLines)
 {
-}
+}*/
 
 }  // namespace RRT
 
