336896621c3d111ae84d78733d44a41351a17f92
Jonathan Bryan Schmalhofer <jonathan.schmalhofer@mytum.de>
2018-08-09 17:54:07 +0200

    added dummy as starting point for recursive stereo ROS node

---
+++ js_recursive_stereo/src/recursive_stereo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def RecursiveStereoNode():
+    pub = rospy.Publisher('pointcloud', String, queue_size=1)
+    rospy.init_node('recursive_stereo', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        RecursiveStereoNode()
+    except rospy.ROSInterruptException:
+        pass
