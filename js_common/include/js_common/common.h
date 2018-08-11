60128bf177068ba716b1ce3775df407e4f6eb146
Jonathan Schmalhofer <jonathan.schmalhofer@mytum.de>
2018-08-11 20:10:46 +0200

    add function for reading boolean params

--- js_common/include/js_common/common.h
+++ js_common/include/js_common/common.h
@@ -11,6 +11,7 @@
 
 namespace js_common
 {
+    // String
     void TryGetParameter(std::string parameter_name, std::string &parameter_value, std::string default_value)
     {
         if (ros::param::has(parameter_name))
@@ -18,6 +19,27 @@ namespace js_common
             if (ros::param::get(parameter_name, parameter_value))
             {
                 ROS_INFO("Got param %s = %s", parameter_name.c_str(), parameter_value.c_str());
+            }
+            else
+            {
+                ROS_ERROR("Failed to get param '%s'", parameter_name.c_str());
+            }
+        }
+        else
+        {
+            ROS_INFO("Using Default Value param %s = %s", parameter_name.c_str(), default_value.c_str());
+            parameter_value = default_value;
+        }
+    }
+    
+    // Bool
+    void TryGetParameter(std::string parameter_name, bool &parameter_value, bool default_value)
+    {
+        if (ros::param::has(parameter_name))
+        {
+            if (ros::param::get(parameter_name, parameter_value))
+            {
+                ROS_INFO("Got param %s = %d", parameter_name.c_str(), parameter_value);
             }
             else
             {
