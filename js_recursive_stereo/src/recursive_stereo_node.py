cf935f0f064024795e85ceb4dc0e0062c1aaec3e
Jonathan Bryan Schmalhofer <jonathan.schmalhofer@mytum.de>
2018-08-11 11:40:23 +0200

    corrected relative paths

--- js_recursive_stereo/src/recursive_stereo_node.py
+++ js_recursive_stereo/src/recursive_stereo_node.py
@@ -4,18 +4,24 @@ import cv2
 import rospy
 from std_msgs.msg import String
 from RecursiveStereoPackage.RecursiveStereo import RecursiveStereo
+import os
+import rospkg
 
 def RecursiveStereoNode():
+    pub = rospy.Publisher('pointcloud', String, queue_size=1)
+    rospy.init_node('recursive_stereo_node', anonymous=True)
+
     algorithm = RecursiveStereo()
     algorithm.Step()
     rospy.loginfo("Loading Images")
-    image_left  = cv2.imread('../resources/left_0000000000.png')
-    image_right = cv2.imread('../resources/right_0000000000.png')
-    algorithm.SetLeftImage(image_left)
-    algorithm.SetRightImage(image_right)
+
+    # get an instance of RosPack with the default search paths
+    rospack = rospkg.RosPack()
+    image_left  = cv2.imread(rospack.get_path('js_recursive_stereo') + '/resources/left_0000000000.png')
+    image_right = cv2.imread(rospack.get_path('js_recursive_stereo') + '/resources/right_0000000000.png')
+    algorithm.left_image  = image_left
+    algorithm.right_image = image_right
     algorithm.Step()
-    pub = rospy.Publisher('pointcloud', String, queue_size=1)
-    rospy.init_node('recursive_stereo', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
