145b6e928707107d55667836a8e607089a37f99f
Jonathan Schmalhofer <jonathan.schmalhofer@mytum.de>
2018-08-18 02:24:19 +0200

    changed library to one large Message StereoImagePose

--- js_airsim_to_ros_library/include/js_airsim_to_ros_library/airsim_to_ros_class.h
+++ js_airsim_to_ros_library/include/js_airsim_to_ros_library/airsim_to_ros_class.h
@@ -9,17 +9,12 @@
 
 #include <cstdint>
 #include <zeromq_cpp/zmq.hpp>
-#include <js_messages/Image_generated.h>
-#include <js_messages/PoseMessageRoot_generated.h>
+//#include <js_messages/Image_generated.h>
+//#include <js_messages/PoseMessageRoot_generated.h>
+#include <js_messages/StereoImagePose_generated.h>
 
 namespace js_airsim_to_ros_library
 {
-
-enum class AirSimToRosType
-{
-    ImageReceiver = 0,
-    PoseReceiver = 1
-};
 
 class AirSimToRosClass
 {
@@ -27,14 +22,8 @@ public:
     /// @brief Initializes a AirSimToRos class instance.
     AirSimToRosClass(std::string const& addr);
     
-    /// @brief Initializes a AirSimToRos class instance.
-    AirSimToRosClass(std::string const& addr, AirSimToRosType type);
-    
     /// @brief Default Destructor to free dynamically allocated memory of received messages.
     ~AirSimToRosClass();
-	
-    /// @brief Set type of receiver
-    void SetType(AirSimToRosType type);
 	
     /// @brief Connect to addr - useful to "reconnect" if connection was lost or could not be established on creation of object
     void Connect(std::string const& addr);
@@ -42,59 +31,59 @@ public:
     // @brief Returns true if a new, full message was received via ZeroMq, false otherwise
     int8_t ReceivedMessage();
     
-    // @brief Returns true if a new, full image message was received
-    int8_t ReceivedImageMessage();
+    // @brief Returns the attribute StereoImagePose.Header.seq
+    uint32_t GetHeaderSeq();
     
-    // @brief Returns true if a new, full image message was received
-    int8_t ReceivedPoseMessage();
+    // @brief Returns the attribute StereoImagePose.stamp.sec
+    uint32_t GetHeaderStampSec();
     
-    // @brief Returns the attribute Image.Header.seq
-    uint32_t GetImageHeaderSeq();
+    // @brief Returns the attribute StereoImagePose.stamp.nsec
+    uint32_t GetHeaderStampNsec();
     
-    // @brief Returns the attribute Image.stamp.sec
-    uint32_t GetImageHeaderStampSec();
+    // @brief Returns the attribute StereoImagePose.frame_id
+    std::string GetHeaderFrameid();
     
-    // @brief Returns the attribute Image.stamp.nsec
-    uint32_t GetImageHeaderStampNsec();
+    // @brief Returns the attribute StereoImagePose.leftheight
+    uint32_t GetLeftHeight();
     
-    // @brief Returns the attribute Image.frame_id
-    std::string GetImageHeaderFrameid();
+    // @brief Returns the attribute StereoImagePose.leftwidth
+    uint32_t GetLeftWidth();
     
-    // @brief Returns the attribute Image.height
-    uint32_t GetImageHeight();
+    // @brief Returns the attribute StereoImagePose.leftencoding
+    std::string GetLeftEncoding();
     
-    // @brief Returns the attribute Image.width
-    uint32_t GetImageWidth();
+    // @brief Returns the attribute StereoImagePose.leftis_bigendian
+    uint8_t GetLeftIsBigendian();
     
-    // @brief Returns the attribute Image.encoding
-    std::string GetImageEncoding();
+    // @brief Returns the attribute StereoImagePose.leftstep
+    uint32_t GetLeftStep();
     
-    // @brief Returns the attribute Image.is_bigendian
-    uint8_t GetImageIsBigendian();
+    // @brief Returns pointer to the attribute StereoImagePose.leftdata
+    std::uint8_t* GetLeftData();
     
-    // @brief Returns the attribute Image.step
-    uint32_t GetImageStep();
+    // @brief Returns the size of the left image data.
+    size_t GetLeftDataSize();
     
-    // @brief Returns pointer to the attribute Image.data
-    std::uint8_t* GetImageData();
+    // @brief Returns the attribute StereoImagePose.right.height
+    uint32_t GetRightHeight();
     
-    // @brief Returns the size of the image data.
-    size_t GetImageDataSize();
+    // @brief Returns the attribute StereoImagePose.right.width
+    uint32_t GetRightWidth();
     
-    // @brief Returns attribute Image.type
-    int8_t GetImageType();
+    // @brief Returns the attribute StereoImagePose.right.encoding
+    std::string GetRightEncoding();
     
-    // @brief Returns the attribute PoseMessage.frame_id
-    std::string GetPoseHeaderFrameid();
+    // @brief Returns the attribute StereoImagePose.right.is_bigendian
+    uint8_t GetRightIsBigendian();
     
-    // @brief Returns the attribute PoseMessage.Header.seq
-    uint32_t GetPoseHeaderSeq();
+    // @brief Returns the attribute StereoImagePose.right.step
+    uint32_t GetRightStep();
     
-    // @brief Returns the attribute PoseMessage.stamp.sec
-    uint32_t GetPoseHeaderStampSec();
+    // @brief Returns pointer to the attribute StereoImagePose.right.data
+    std::uint8_t* GetRightData();
     
-    // @brief Returns the attribute PoseMessage.stamp.nsec
-    uint32_t GetPoseHeaderStampNsec();
+    // @brief Returns the size of the StereoImagePose.right data.
+    size_t GetRightDataSize();
     
     // @brief Returns the attribute PoseMessage.position.x
     double GetPosePositionX();
@@ -114,63 +103,66 @@ public:
     // @brief Returns the attribute PoseMessage.orientation.yaw
     double GetPoseOrientationYaw();
 
-private:
-    /// @brief Type of receiver - either for receiving images or receiving poses
-    AirSimToRosType receiver_type_;
-    
+private:    
     /// @brief A ZeroMq context object encapsulating functionality dealing with the initialisation and termination.
     zmq::context_t zmq_context_;
     
     /// @brief A ZeroMq socket for subscribing to incoming messages.
     zmq::socket_t zmq_subscriber_;
 
-    /// @brief Holds the attribute Image.Header.seq of the last received message.
-    uint32_t image_header_seq_;
+    /// @brief Holds the attribute StereoImagePose.Header.seq of the last received message.
+    uint32_t header_seq_;
 
-    /// @brief Holds the attribute Image.Header.stamp.sec of the last received message.
-    uint32_t image_header_stamp_sec_;
+    /// @brief Holds the attribute StereoImagePose.Header.stamp.sec of the last received message.
+    uint32_t header_stamp_sec_;
 
-    /// @brief Holds the attribute Image.Header.stamp.sec of the last received message.
-    uint32_t image_header_stamp_nsec_;
+    /// @brief Holds the attribute StereoImagePose.Header.stamp.sec of the last received message.
+    uint32_t header_stamp_nsec_;
 
-    /// @brief Holds the attribute Image.Header.frame_id of the last received message.
-    std::string image_header_frame_id_;
+    /// @brief Holds the attribute StereoImagePose.Header.frame_id of the last received message.
+    std::string header_frame_id_;
 
-    /// @brief Holds the attribute Image.height of the last received message.
-    uint32_t image_height_;
+    /// @brief Holds the attribute StereoImagePose.left.height of the last received message.
+    uint32_t left_height_;
 
-    /// @brief Holds the attribute Image.width of the last received message.
-    uint32_t image_width_;
+    /// @brief Holds the attribute StereoImagePose.left.width of the last received message.
+    uint32_t left_width_;
 
-    /// @brief Holds the attribute Image.encoding of the last received message.
-    std::string image_encoding_;
+    /// @brief Holds the attribute StereoImagePose.left.encoding of the last received message.
+    std::string left_encoding_;
 
-    /// @brief Holds the attribute Image.is_bigendian of the last received message.
-    uint8_t image_is_bigendian_;
+    /// @brief Holds the attribute StereoImagePose.left.is_bigendian of the last received message.
+    uint8_t left_is_bigendian_;
 
-    /// @brief Holds the attribute Image.step of the last received message.
-    uint32_t image_step_;
+    /// @brief Holds the attribute StereoImagePose.left.step of the last received message.
+    uint32_t left_step_;
     
-    /// @brief Holds the attribute Image.data of the last received message.
-    std::uint8_t* image_data_;
+    /// @brief Holds the attribute StereoImagePose.left.data of the last received message.
+    std::uint8_t* left_data_;
     
-    /// @brief Holds the size of the image data (which is step * height).
-    size_t image_data_size_;    
+    /// @brief Holds the size of the StereoImagePose.left data (which is step * height).
+    size_t left_data_size_;
 
-    /// @brief Holds the attribute Image.type of the last received message.
-    int8_t image_type_;
+    /// @brief Holds the attribute StereoImagePose.right.height of the last received message.
+    uint32_t right_height_;
+
+    /// @brief Holds the attribute StereoImagePose.right.width of the last received message.
+    uint32_t right_width_;
+
+    /// @brief Holds the attribute StereoImagePose.right.encoding of the last received message.
+    std::string right_encoding_;
+
+    /// @brief Holds the attribute StereoImagePose.right.is_bigendian of the last received message.
+    uint8_t right_is_bigendian_;
+
+    /// @brief Holds the attribute StereoImagePose.right.step of the last received message.
+    uint32_t right_step_;
     
-    /// @brief Holds the attribute PoseMessage.Header.seq of the last received message.
-    uint32_t pose_header_seq_;
-
-    /// @brief Holds the attribute PoseMessage.Header.stamp.sec of the last received message.
-    uint32_t pose_header_stamp_sec_;
-
-    /// @brief Holds the attribute PoseMessage.Header.stamp.sec of the last received message.
-    uint32_t pose_header_stamp_nsec_;
-
-    /// @brief Holds the attribute PoseMessage.Header.frame_id of the last received message.
-    std::string pose_header_frame_id_;
+    /// @brief Holds the attribute StereoImagePose.right.data of the last received message.
+    std::uint8_t* right_data_;
+    
+    /// @brief Holds the size of the StereoImagePose.right data (which is step * height).
+    size_t right_data_size_;
 
     /// @brief Holds the attribute PoseMessage.position.x of the last received message.
     double pose_position_x_;
--- js_airsim_to_ros_library/src/airsim_to_ros_class.cpp
+++ js_airsim_to_ros_library/src/airsim_to_ros_class.cpp
@@ -19,71 +19,33 @@ AirSimToRosClass::AirSimToRosClass(std::string const& addr)
     zmq_subscriber_.setsockopt(ZMQ_RCVTIMEO, 5000);
     zmq_subscriber_.connect(addr);
     
-    // Image.Header
-    image_header_seq_           = 0;
-    image_header_stamp_sec_     = 0;
-    image_header_stamp_nsec_    = 0;
-    image_header_frame_id_      = "";
-    // Image
-    image_height_               = 0;
-    image_width_                = 0;
-    image_encoding_             = "";
-    image_is_bigendian_         = 0;
-    image_step_                 = 0;
-    image_data_                 = NULL;
-    image_data_size_            = 0;
-    image_type_                 = 0;
+    // Header
+    header_seq_                 = 0;
+    header_stamp_sec_           = 0;
+    header_stamp_nsec_          = 0;
+    header_frame_id_            = "";
+    // Left
+    left_height_                = 0;
+    left_width_                 = 0;
+    left_encoding_              = "";
+    left_is_bigendian_          = 0;
+    left_step_                  = 0;
+    left_data_                  = NULL;
+    left_data_size_             = 0;
+    // Right
+    right_height_               = 0;
+    right_width_                = 0;
+    right_encoding_             = "";
+    right_is_bigendian_         = 0;
+    right_step_                 = 0;
+    right_data_                 = NULL;
+    right_data_size_            = 0;
     
-    // PoseMessage.Header
-    pose_header_seq_            = 0;
-    pose_header_stamp_sec_      = 0;
-    pose_header_stamp_nsec_     = 0;
-    pose_header_frame_id_       = "";
-    // PoseMessage.position
+    // Pose.position
     pose_position_x_            = 0;
     pose_position_y_            = 0;
     pose_position_z_            = 0;
-    // PoseMessage.orientation
-    pose_orientation_roll_      = 0;
-    pose_orientation_pitch_     = 0;
-    pose_orientation_yaw_       = 0;
-}
-
-AirSimToRosClass::AirSimToRosClass(std::string const& addr, AirSimToRosType type)
-    : zmq_context_(1)
-    , zmq_subscriber_(zmq_context_, ZMQ_SUB)
-{
-    receiver_type_ = type;
-    zmq_subscriber_.setsockopt(ZMQ_IDENTITY, "AirSimToRosSubscriber", 5);
-    zmq_subscriber_.setsockopt(ZMQ_SUBSCRIBE, "", 0);
-    zmq_subscriber_.setsockopt(ZMQ_RCVTIMEO, 5000);
-    zmq_subscriber_.connect(addr);
-    
-    // Image.Header
-    image_header_seq_           = 0;
-    image_header_stamp_sec_     = 0;
-    image_header_stamp_nsec_    = 0;
-    image_header_frame_id_      = "";
-    // Image
-    image_height_               = 0;
-    image_width_                = 0;
-    image_encoding_             = "";
-    image_is_bigendian_         = 0;
-    image_step_                 = 0;
-    image_data_                 = NULL;
-    image_data_size_            = 0;
-    image_type_                 = 0;
-    
-    // PoseMessage.Header
-    pose_header_seq_            = 0;
-    pose_header_stamp_sec_      = 0;
-    pose_header_stamp_nsec_     = 0;
-    pose_header_frame_id_       = "";
-    // PoseMessage.position
-    pose_position_x_            = 0;
-    pose_position_y_            = 0;
-    pose_position_z_            = 0;
-    // PoseMessage.orientation
+    // Pose.orientation
     pose_orientation_roll_      = 0;
     pose_orientation_pitch_     = 0;
     pose_orientation_yaw_       = 0;
@@ -96,39 +58,17 @@ void AirSimToRosClass::Connect(std::string const& addr)
     
 AirSimToRosClass::~AirSimToRosClass()
 {
-    free(image_data_);
-}
-
-void AirSimToRosClass::SetType(AirSimToRosType type)
-{
-    receiver_type_ = type;
+    free(left_data_);
+    free(right_data_);
 }
 
 int8_t AirSimToRosClass::ReceivedMessage()
 {
-    int8_t return_value = 0;
-    if(receiver_type_ == AirSimToRosType::ImageReceiver)
-    {
-        return_value = ReceivedImageMessage();
-    }
-    else if(receiver_type_ == AirSimToRosType::PoseReceiver)
-    {
-        return_value = ReceivedPoseMessage();
-    }
-    else
-    {
-        return_value = -1;
-    }
-    return return_value;
-}
-
-int8_t AirSimToRosClass::ReceivedImageMessage()
-{
     zmq::message_t zmq_received_message;
     zmq_subscriber_.recv(&zmq_received_message);
 
     flatbuffers::FlatBufferBuilder fbb;
-    airsim_to_ros::ImageBuilder builder(fbb);
+    airsim_to_ros::StereoImagePoseBuilder builder(fbb);
 
     // Verify the message contents are trustworthy.
     flatbuffers::Verifier verifier((const unsigned char *)zmq_received_message.data(),
@@ -139,72 +79,42 @@ int8_t AirSimToRosClass::ReceivedImageMessage()
     if (zmq_received_message.size() > 0)
     {
       // Make sure the message has valid data.
-      if (airsim_to_ros::VerifyImageBuffer(verifier))
+      if (airsim_to_ros::VerifyStereoImagePoseBuffer(verifier))
       {
-          auto flatbuffer_image_rcvd = airsim_to_ros::GetImage(zmq_received_message.data());
+          auto flatbuffer_rcvd = airsim_to_ros::GetStereoImagePose(zmq_received_message.data());
 
           // Header
-          image_header_seq_           = flatbuffer_image_rcvd->header()->seq();
-          image_header_stamp_sec_     = flatbuffer_image_rcvd->header()->stamp()->sec();
-          image_header_stamp_nsec_    = flatbuffer_image_rcvd->header()->stamp()->nsec();
-          image_header_frame_id_      = flatbuffer_image_rcvd->header()->frame_id()->c_str();
-          // Image
-          image_height_               = flatbuffer_image_rcvd->height();
-          image_width_                = flatbuffer_image_rcvd->width();
-          image_encoding_             = flatbuffer_image_rcvd->encoding()->c_str();
-          image_is_bigendian_         = flatbuffer_image_rcvd->is_bigendian();
-          image_step_                 = flatbuffer_image_rcvd->step();
-          image_data_                 = (std::uint8_t*)realloc(image_data_, zmq_received_message.size());
-          image_data_size_            = image_step_ * image_height_;
-          memcpy(image_data_, flatbuffer_image_rcvd->data(), image_data_size_);
-          image_type_                 = flatbuffer_image_rcvd->type();
-          return_value                = 1;
-      }
-      else
-      {
-          return_value                = -1;
-      }
-    }
-
-    return return_value;
-}
-
-
-
-int8_t AirSimToRosClass::ReceivedPoseMessage()
-{
-    zmq::message_t zmq_received_message;
-    zmq_subscriber_.recv(&zmq_received_message);
-
-    flatbuffers::FlatBufferBuilder fbb;
-    airsim_to_ros::PoseMessageBuilder builder(fbb);
-
-    // Verify the message contents are trustworthy.
-    flatbuffers::Verifier verifier((const unsigned char *)zmq_received_message.data(),
-                                                          zmq_received_message.size());
-    int8_t return_value = 0;
-
-
-    if (zmq_received_message.size() > 0)
-    {
-      // Make sure the message has valid data.
-      if (airsim_to_ros::VerifyPoseMessageBuffer(verifier))
-      {
-          auto flatbuffer_pose_rcvd = airsim_to_ros::GetPoseMessage(zmq_received_message.data());
-
-          // Header
-          pose_header_seq_            = flatbuffer_pose_rcvd->header()->seq();
-          pose_header_stamp_sec_      = flatbuffer_pose_rcvd->header()->stamp()->sec();
-          pose_header_stamp_nsec_     = flatbuffer_pose_rcvd->header()->stamp()->nsec();
-          pose_header_frame_id_       = flatbuffer_pose_rcvd->header()->frame_id()->c_str();
+          header_seq_                 = flatbuffer_rcvd->header()->seq();
+          header_stamp_sec_           = flatbuffer_rcvd->header()->stamp()->sec();
+          header_stamp_nsec_          = flatbuffer_rcvd->header()->stamp()->nsec();
+          header_frame_id_            = flatbuffer_rcvd->header()->frame_id()->c_str();
+          // Left
+          left_height_               = flatbuffer_rcvd->left()->height();
+          left_width_                = flatbuffer_rcvd->left()->width();
+          left_encoding_             = flatbuffer_rcvd->left()->encoding()->c_str();
+          left_is_bigendian_         = flatbuffer_rcvd->left()->is_bigendian();
+          left_step_                 = flatbuffer_rcvd->left()->step();
+          left_data_                 = (std::uint8_t*)realloc(left_data_, left_step_ * left_height_);//zmq_received_message.size());
+          left_data_size_            = left_step_ * left_height_;
+          memcpy(left_data_, flatbuffer_rcvd->left()->data(), left_data_size_);
+          // Right
+          right_height_               = flatbuffer_rcvd->right()->height();
+          right_width_                = flatbuffer_rcvd->right()->width();
+          right_encoding_             = flatbuffer_rcvd->right()->encoding()->c_str();
+          right_is_bigendian_         = flatbuffer_rcvd->right()->is_bigendian();
+          right_step_                 = flatbuffer_rcvd->right()->step();
+          right_data_                 = (std::uint8_t*)realloc(right_data_, right_step_ * right_height_);//zmq_received_message.size());
+          right_data_size_            = right_step_ * right_height_;
+          memcpy(right_data_, flatbuffer_rcvd->right()->data(), right_data_size_);
+          
           // position
-          pose_position_x_            = flatbuffer_pose_rcvd->position()->x();
-          pose_position_y_            = flatbuffer_pose_rcvd->position()->y();
-          pose_position_z_            = flatbuffer_pose_rcvd->position()->z();
+          pose_position_x_            = flatbuffer_rcvd->pose()->position()->x();
+          pose_position_y_            = flatbuffer_rcvd->pose()->position()->y();
+          pose_position_z_            = flatbuffer_rcvd->pose()->position()->z();
           // orientation
-          pose_orientation_roll_      = flatbuffer_pose_rcvd->orientation()->roll();
-          pose_orientation_pitch_     = flatbuffer_pose_rcvd->orientation()->pitch();
-          pose_orientation_yaw_       = flatbuffer_pose_rcvd->orientation()->yaw();
+          pose_orientation_roll_      = flatbuffer_rcvd->pose()->orientation()->roll();
+          pose_orientation_pitch_     = flatbuffer_rcvd->pose()->orientation()->pitch();
+          pose_orientation_yaw_       = flatbuffer_rcvd->pose()->orientation()->yaw();
           return_value                = 1;
       }
       else
@@ -216,84 +126,94 @@ int8_t AirSimToRosClass::ReceivedPoseMessage()
     return return_value;
 }
     
-uint32_t AirSimToRosClass::GetImageHeaderSeq()
+uint32_t AirSimToRosClass::GetHeaderSeq()
 {
-    return image_header_seq_;
+    return header_seq_;
 }
     
-uint32_t AirSimToRosClass::GetImageHeaderStampSec()
+uint32_t AirSimToRosClass::GetHeaderStampSec()
 {
-    return image_header_stamp_sec_;
+    return header_stamp_sec_;
 }
 
-uint32_t AirSimToRosClass::GetImageHeaderStampNsec()
+uint32_t AirSimToRosClass::GetHeaderStampNsec()
 {
-    return image_header_stamp_nsec_;
+    return header_stamp_nsec_;
 }
 
-std::string AirSimToRosClass::GetImageHeaderFrameid()
+std::string AirSimToRosClass::GetHeaderFrameid()
 {
-    return image_header_frame_id_;
+    return header_frame_id_;
 }
 
-uint32_t AirSimToRosClass::GetImageHeight()
+uint32_t AirSimToRosClass::GetLeftHeight()
 {
-    return image_height_;
+    return left_height_;
 }
 
-uint32_t AirSimToRosClass::GetImageWidth()
+uint32_t AirSimToRosClass::GetLeftWidth()
 {
-    return image_width_;
+    return left_width_;
 }
 
-std::string AirSimToRosClass::GetImageEncoding()
+std::string AirSimToRosClass::GetLeftEncoding()
 {
-    return image_encoding_;
+    return left_encoding_;
 }
 
-uint8_t AirSimToRosClass::GetImageIsBigendian()
+uint8_t AirSimToRosClass::GetLeftIsBigendian()
 {
-    return image_is_bigendian_;
+    return left_is_bigendian_;
 }
 
-uint32_t AirSimToRosClass::GetImageStep()
+uint32_t AirSimToRosClass::GetLeftStep()
 {
-    return image_step_;
+    return left_step_;
 }
 
-std::uint8_t* AirSimToRosClass::GetImageData()
+std::uint8_t* AirSimToRosClass::GetLeftData()
 {
-    return image_data_;
+    return left_data_;
 }
     
-size_t AirSimToRosClass::GetImageDataSize()
+size_t AirSimToRosClass::GetLeftDataSize()
 {
-    return image_data_size_;
-}
-    
-int8_t AirSimToRosClass::GetImageType()
-{
-    return image_type_;
-}
-    
-uint32_t AirSimToRosClass::GetPoseHeaderSeq()
-{
-    return pose_header_seq_;
-}
-    
-uint32_t AirSimToRosClass::GetPoseHeaderStampSec()
-{
-    return pose_header_stamp_sec_;
+    return left_data_size_;
 }
 
-uint32_t AirSimToRosClass::GetPoseHeaderStampNsec()
+uint32_t AirSimToRosClass::GetRightHeight()
 {
-    return pose_header_stamp_nsec_;
+    return right_height_;
 }
 
-std::string AirSimToRosClass::GetPoseHeaderFrameid()
+uint32_t AirSimToRosClass::GetRightWidth()
 {
-    return pose_header_frame_id_;
+    return right_width_;
+}
+
+std::string AirSimToRosClass::GetRightEncoding()
+{
+    return right_encoding_;
+}
+
+uint8_t AirSimToRosClass::GetRightIsBigendian()
+{
+    return right_is_bigendian_;
+}
+
+uint32_t AirSimToRosClass::GetRightStep()
+{
+    return right_step_;
+}
+
+std::uint8_t* AirSimToRosClass::GetRightData()
+{
+    return right_data_;
+}
+    
+size_t AirSimToRosClass::GetRightDataSize()
+{
+    return right_data_size_;
 }
 
 double AirSimToRosClass::GetPosePositionX()
